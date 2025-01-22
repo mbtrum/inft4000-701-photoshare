f9f11cc2c7eb62897d9b75853291fbb0d0d01531
Mike Trumbull <michael.trumbull@nscc.ca>
2025-01-22 16:34:34 -0400

    small fix

--- PhotoShare/Controllers/HomeController.cs
+++ PhotoShare/Controllers/HomeController.cs
@@ -16,6 +16,29 @@ namespace PhotoShare.Controllers
             // create a list of photos
             List<Photo> photos = new List<Photo>();
 
+            // create 3 photos
+            Photo photo1 = new Photo();
+            photo1.PhotoId = 1;
+            photo1.Description = "A photo of my dog.";
+            photo1.CreatedAt = DateTime.Now;
+            photo1.ImageFilename = "dog.jpg";
+
+            Photo photo2 = new Photo();
+            photo2.PhotoId = 2;
+            photo2.Description = "A photo of my cat.";
+            photo2.CreatedAt = DateTime.Now;
+            photo2.ImageFilename = "cat.jpg";
+
+            Photo photo3 = new Photo();
+            photo3.PhotoId = 3;
+            photo3.Description = "A photo of my parrot.";
+            photo3.CreatedAt = DateTime.Now;
+            photo3.ImageFilename = "parrot.jpg";
+
+            // add to list
+            photos.Add(photo1);
+            photos.Add(photo2);
+            photos.Add(photo3);
 
             // Pass photos object into View
             return View(photos);
